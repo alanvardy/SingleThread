712a5cf0c0648807ad567cba0d32af8cd9511764
Alan Vardy <alanvardy@gmail.com>
2026-08-13 17:40:10 -0700

    Fetch one reminder

--- SingleThread/SingleThreadApp.swift
+++ SingleThread/SingleThreadApp.swift
@@ -1,32 +1,10 @@
-//
-//  SingleThreadApp.swift
-//  SingleThread
-//
-//  Created by Alan Vardy on 2026-08-12.
-//
-
-import SwiftData
 import SwiftUI
 
 @main
 struct SingleThreadApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(sharedModelContainer)
     }
 }
